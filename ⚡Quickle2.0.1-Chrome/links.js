84b643d2bee89eb625890ba06895ca2f97bd71cb
Gabe <67393936+gabefox07@users.noreply.github.com>
2021-11-22 09:36:44 -0600

    11/22/21

---
+++ ⚡Quickle2.0.1-Chrome/links.js
@@ -0,0 +1,2 @@
+let homebutton = document.getElementById('home-btn');
+homebutton.style.backgroundColor = '#e8e8e8';
---
+++ ⚡Quickle2.0.1-Chrome/settings.js
@@ -0,0 +1,2 @@
+let settingsbutton = document.getElementById('settings-btn');
+settingsbutton.style.backgroundColor = '#e8e8e8';
